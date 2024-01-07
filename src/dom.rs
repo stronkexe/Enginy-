c05c879980db1ce3f55a263ba9ccba46d8111183
stronkexe <elasri9999@gmail.com>
2024-01-07 01:15:52 +0100

    DOM and HTML Parser initialization.

---
+++ src/dom.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use std::fmt;
+
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+    Comment(String),
+}
+
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+impl Node {
+    fn new(node_type: NodeType, children: Vec<Node>) -> Node {
+        Node {
+            node_type,
+            children,
+        }
+    }
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.node_type)
+    }
+}
